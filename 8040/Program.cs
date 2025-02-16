0f4536118111fc850d453fcf63af1f590302b72d
Gideon Yuval <gideonyuval@gmail.com>
2025-02-16 07:39:24 +0200

    added basic part

--- 8040/Program.cs
+++ 8040/Program.cs
@@ -10,33 +10,36 @@ namespace _8040
     {
         static void Main(string[] args)
         {
-            Animal a1 = new Animal();
-            Dog d1 = new Dog();
             Cat c1 = new Cat();
             c1.MakeNoise();
             c1.MakeNoise(3);
 
-            //DoAnimalStuff(a1);
-            //DoAnimalStuff(d1);
-            //DoAnimalStuff(c1);
+            Animal a1 = new Dog(); //(Animal)new Dog(); isn't needed
+            //Dog d2 = new Animal(); //can't do this
+            //Dog d2 = (Dog)(new Animal());
 
-            Console.WriteLine("demo");
-            Animal[] arr = new Animal[3];
-            arr[0] = a1;
-            arr[1] = d1;
-            arr[2] = c1;
-            foreach (Animal a2 in arr)
-                a2.MakeNoise();
+            a1.MakeNoise();
+            //a1.Growl(); //a1 doesn't have access to Growl
 
 
-            Console.WriteLine(d1 is Animal);
+
+            //a1 is declared as an Animal, but it actually refers to a Dog object
+            //The is operator checks the actual type of the object at runtime, not the reference type.
             Console.WriteLine(a1 is Dog);
+            Console.WriteLine(a1.GetType().Name);
 
-            Animal a = new Dog(); //no need to write explicitly (Animal) new Dog();
-            a.MakeNoise();
 
-            ((Dog)a).Growl();
-            //((Dog)c1).Growl(); //can't do this
+            ((Dog)a1).Growl(); //remember the double ((
+
+            //Dog d1 = a1; //fails
+
+            //a1.Jump(); //won't compile, a1 is an Animal, which doesn't implement Jump()
+            a1.Jump(3); //calls the Animal method 
+            ((Dog)a1).Jump(); //calls Dog's implentation, as we're downcasting a1 
+            
+            ((Dog)a1).Jump(3); //Dog inherits Jump(int jumps)
+
+
 
 
         }
@@ -57,6 +60,11 @@ namespace _8040
         {
             Console.WriteLine("Animal Noise");
         }
+
+        public void Jump(int jumps)
+        {
+            Console.WriteLine($"jump {jumps} times");
+        }
     }
 
     public class Dog : Animal
@@ -70,6 +78,11 @@ namespace _8040
         {
             Console.WriteLine("Grrrr");
         }
+
+        public void Jump()
+        {
+            Console.WriteLine("Jump!");
+        }
     }
 
     public class Cat : Animal
@@ -80,11 +93,5 @@ namespace _8040
         }
     }
 
-    public class Car
-    {
-        public void MakeNoise()
-        {
-            Console.WriteLine("Voom!");
-        }
-    }
+
 }
